f38cf4dd5814d305ed5edb09cbb6c23d52639775
Sean Perinuzzi <sperinuzzi@gmail.com>
2022-08-21 20:12:04 -0400

    corrected two different names that might have caused a problem in the code

--- ai.py
+++ ai.py
@@ -7,9 +7,8 @@ class Ai(Player):
     
     def choose_gesture(self):
         self.chosen_gesture = random.choice(self.gesture)
-        print(f'C3PO chose {self.chosen_gesture}')
+        print(f'Mr.Roboto chose {self.chosen_gesture}')
         return self.chosen_gesture
         
     def player_name(self):
        self.name = 'Mr.Roboto'
-       
